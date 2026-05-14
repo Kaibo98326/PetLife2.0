9f77495420c437a6e596c81f71606ed546af7e6e
Kaibo <kaibo98326@gmail.com>
2026-05-14 16:39:08 +0800

    增加顯示角色綁定名稱 ， 修正後台登出順序 SWalert -> 清除emp資訊 -> SWalert  -> 回login

--- frontend-admin/src/layout/Header.vue
+++ frontend-admin/src/layout/Header.vue
@@ -11,7 +11,7 @@
         </div>
         <div class="user-text-info">
           <span class="user-name">{{ employeeStore.empName }}</span>
-          <span class="user-role">{{ employeeStore.role || '系統管理員' }}</span>
+          <span class="user-role">{{ displayRole}}</span>
         </div>
       </div>
       <button class="logout-btn" @click="logout">登出</button>
@@ -20,6 +20,7 @@
 </template>
 
 <script setup>
+import { computed } from 'vue';
 import { useEmployeeStore } from '@/stores/employee';
 import { useRouter } from 'vue-router';
 import Swal from 'sweetalert2';
@@ -27,16 +28,36 @@ import Swal from 'sweetalert2';
 const employeeStore = useEmployeeStore();
 const router = useRouter();
 
+const displayRole = computed(() => {
+  const roles = employeeStore.roles || []
+
+  if (roles.includes('superuser')) {
+    return '後台管理員'
+  }
+
+  if (roles.includes('staff')) {
+    return '一般員工'
+  }
+
+  if (roles.includes('groomer')) {
+    return '美容師'
+  }
+
+  return '未設定角色'
+})
+
 const logout = () => {
-  employeeStore.logout();
   Swal.fire({
-        icon: 'warning',
-        title: '確定要登出嗎？',
-        text: '登出後需要重新登入才能使用後台系統功能',
-        showCancelButton: true,
-        confirmButtonText: '是的，登出',
-        cancelButtonText: '取消'
-    }).then((result) => {
+    icon: 'warning',
+    title: '確定要登出嗎？',
+    text: '登出後需要重新登入才能使用後台系統功能',
+    showCancelButton: true,
+    confirmButtonText: '是的，登出',
+    cancelButtonText: '取消'
+  }).then((result) => {
+        if(!result.isConfirmed) return;
+
+        employeeStore.logout();
         Swal.fire({
                 icon: 'success',
                 title: '已登出',
